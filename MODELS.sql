e547aa955083b44f61d2b8bf201a8a0e4b5376ef
kazuma.itou <brightdelusion@gmail.com>
2023-01-19 18:39:39 +0900

    fix: 모델 UUID

--- MODELS.sql
+++ MODELS.sql
@@ -1,65 +1,68 @@
+BEGIN;
 INSERT INTO `models` (`id`, `name`, `port`, `vendor`, `web_port`) VALUES
-(‘EABEBC7CECF34A’, ‘MDHF-IES-L4000-12P8G4SF’, 4, ‘HYESUNG’, 80);
+(X'E1C343F342C44B21BFDC2C6DEFC09254', 'MDHF-IES-L4000-12P8G4SF', 4, 'HYESUNG', 80);
 -- 2023-01-18 06:24:39.5608
 INSERT INTO `models` (`id`, `name`, `port`, `vendor`, `web_port`) VALUES
-(‘EABEBC7CECF34B’, ‘MDHF-IES-L4000-14P8G6SF’, 6, ‘HYESUNG’, 80);
+(X'A1BA73BBC03144A1ABE80267414E74CE', 'MDHF-IES-L4000-14P8G6SF', 6, 'HYESUNG', 80);
 -- 2023-01-18 06:28:07.4315
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘408’, ‘version’, ‘SNMP’, ‘1’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), 'version', 'SNMP', '1', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4315
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘409’, ‘community’, ‘SNMP’, ‘public’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), 'community', 'SNMP', 'public', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4322
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘410’, ‘count’, ‘SNMP’, ‘4’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), 'count', 'SNMP', '4', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4327
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘411’, ‘1’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000009’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '1', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000009', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4327
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘412’, ‘2’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000010’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '2', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000010', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4329
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘413’, ‘3’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000011’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '3', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000011', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4329
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘414’, ‘4’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000012’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '4', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000012', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4329
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘415’, ‘1’, ‘POWER’, ‘.1.3.6.1.4.1.43866.1.1.1.9.0’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '1', 'POWER', '.1.3.6.1.4.1.43866.1.1.1.9.0', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:28:07.4329
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘416’, ‘2’, ‘POWER’, ‘.1.3.6.1.4.1.43866.1.1.1.10.0’, ‘EABEBC7CECF34A’);
+(uuid_to_bin(UUID()), '2', 'POWER', '.1.3.6.1.4.1.43866.1.1.1.10.0', X'E1C343F342C44B21BFDC2C6DEFC09254');
 -- 2023-01-18 06:31:19.6353
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘417’, ‘version’, ‘SNMP’, ‘1’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), 'version', 'SNMP', '1', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘418’, ‘community’, ‘SNMP’, ‘public’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), 'community', 'SNMP', 'public', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘419’, ‘count’, ‘SNMP’, ‘6’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), 'count', 'SNMP', '6', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘420’, ‘1’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000009’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '1', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000009', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘421’, ‘2’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000010’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '2', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000010', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘422’, ‘3’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000011’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '3', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000011', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘423’, ‘4’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000012’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '4', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000012', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6363
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘424’, ‘5’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000013’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '5', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000013', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6373
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘425’, ‘6’, ‘LINK’, ‘.1.3.6.1.2.1.2.2.1.8.1000014’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '6', 'LINK', '.1.3.6.1.2.1.2.2.1.8.1000014', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6373
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘426’, ‘1’, ‘POWER’, ‘.1.3.6.1.4.1.43866.1.1.1.9.0’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '1', 'POWER', '.1.3.6.1.4.1.43866.1.1.1.9.0', X'A1BA73BBC03144A1ABE80267414E74CE');
 -- 2023-01-18 06:31:19.6373
 INSERT INTO `model_properties` (`id`, `key`, `type`, `value`, `model_id`) VALUES
-(‘427’, ‘2’, ‘POWER’, ‘.1.3.6.1.4.1.43866.1.1.1.10.0’, ‘EABEBC7CECF34B’);
+(uuid_to_bin(UUID()), '2', 'POWER', '.1.3.6.1.4.1.43866.1.1.1.10.0', X'A1BA73BBC03144A1ABE80267414E74CE');
+
+COMMIT;
